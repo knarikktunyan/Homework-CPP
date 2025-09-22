3a58904ad54d56db0888e085ab9573ad8ae81b08
knarikktunyan <knarikktunyan@gmail.com>
2025-09-22 22:35:40 +0400

    changes

--- hmw1.cpp
+++ hmw1.cpp
@@ -1,30 +1,17 @@
 #include <iostream>
-int main()
-{
-    int i;
-    int arr[] = {1, 2,4, 4, 3, 1, 2};
+using namespace std;
+
+int main() {
+    int arr[] = {1, 2, 4, 4, 3, 1, 2};
     int n = sizeof(arr) / sizeof(arr[0]);
-    bool isDub = false;
 
-    for(i = 0; i<n; i++)
-    {
-        bool isDub = false;
+    int result = 0;
 
-        for(int j = 0; j < n; j++)
-        { 
-            if(arr[i] == arr[j] && i != j)
-            {
-                isDub=true;
-                break;
-            }
-        }
-        if(!isDub)
-        {
-            std::cout << arr[i] << " "<<std::endl;
-            break;
-        }
+    for (int i = 0; i < n; i++) {
+        result ^= arr[i];
+    }
 
-            
-    }         
+    cout << "unique element: " << result << endl;
+
     return 0;
 }
